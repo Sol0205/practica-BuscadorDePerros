87ffa0ecb2458fdddb1f6ee32820d6c4fd55e7a8
Sol0205 <87732422+Sol0205@users.noreply.github.com>
2021-12-26 11:41:43 -0300

    Crear layout (#3)
    - agrego css global
    -  agrego layout global
    - instalo bootstrap y agrego al proyecto
    - agrego html global
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import Layout from './components/Layout/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
