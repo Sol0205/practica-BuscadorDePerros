d9bad69984e513d02e2d6f79c8056b7303a0a1da
Sol0205 <solmargarit3@gmail.com>
2021-12-26 11:38:04 -0300

    - agrego css global -  agrego layout global - instalo bootstrap y agrego al proyecto - agrego html global

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,1 +1,56 @@
-console.log();
+import * as React from "react";
+
+function Layout() {
+  return (
+    <div className="App">
+      <header>
+        <div className="container">
+          <div className="row">
+            <Logo />
+            <SearchBar />
+          </div>
+        </div>
+      </header>
+
+      <main className="flex-shrink-0">
+        <div class="container">
+          <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+         
+        </div>
+        {/* <DogCatalog /> */}
+        {/* <aside>
+                   <FilterBar />
+               </aside>
+
+                    <CardList /> */}
+      </main>
+
+
+    </div>
+  );
+}
+
+export default Layout;
+
+function SearchBar() {
+  return (
+    <div className="col-4">
+      <form className="d-flex">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+      </form>
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="col-8">
+      <h1>Buscador de perros</h1>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './components/Layout/Layout';
-import 'bootstrap/t/css/bootstrap.min.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
