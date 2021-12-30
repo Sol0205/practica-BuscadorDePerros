5dd59e657173de1bd5781e136064a568af1e6b16
Sol0205 <87732422+Sol0205@users.noreply.github.com>
2021-12-30 18:29:38 -0300

    Feature/crear cards component (#8)
    * - instale prop-types
    - cree las cards
    - elimino estilos deprecados
    
    * - hice mejoras a las cards
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,34 @@
-import React from 'react'; 
+import React from "react";
+import PropTypes from "prop-types";
 import './Card.css';
 
-function Card() {
-    return (
-    <div className="card">
-        <img src="https://picsum.photos/id/237/200" className="card-img-top" alt="foto de perro beagle" />
-        <div className="card-body">
-            <h4 className="card-title">Beagle</h4>
-            {/* text-primary || text-secundary || text-danger || text-warning */}
-            <p className= "card-text text-secundary">Altura: 36 cm.</p>
-            <p className= "card-text text-secundary">Peso: 12 kg</p>
-            <a href="#" className="btn btn-primary">Ver mas.</a>
+function Card(props) {
+  return (
+    <div className="card bg-light" >
+      <img src={props.cardInfo.img} alt="" />
+      <div className="card-body text-dark">
+        <h3 className="card-title align-baseline fw-bolder fs-4">{props.cardInfo.title}</h3>
+        {/* text-primary || text-secundary || text-danger || text-warning */}
+        <br />
+        <p className="card-text text-secundary align-baseline fs-5">{props.cardInfo.altura}</p>
+        <p className="card-text text-secundary align-baseline fs-5">{props.cardInfo.peso}</p>
+        <div className="row">
+          <a href="https://www.hillspet.es/dog-care/dog-breeds/beagle" className="btn btn-primary col align-self-end fs-5">
+            Ver mas.
+          </a>
         </div>
+      </div>
     </div>
-    )
+  );
 }
 
-export default Card
+Card.propTypes = {
+  cardInfo: PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    altura: PropTypes.string.isRequired,
+    peso: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Card;
