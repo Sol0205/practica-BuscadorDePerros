e6affdb96056fa1d71acbcd67e2e1b4d063f8fbb
Sol0205 <87732422+Sol0205@users.noreply.github.com>
2022-01-02 01:26:09 -0300

    Feachure/agregar prop types al buttom custom (#10) (#11)
    * Crear layout (#3)
    
    
    - agrego css global
    -  agrego layout global
    - instalo bootstrap y agrego al proyecto
    - agrego html global
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>
    
    * Feature/crear search bar componet (#4)
    
    * Modifico el layout para ordenar mejor la pagina
    
    * Hago una card
    
    * - instale reactstrap, jquery y popper
    - cree el dropdown
    
    * - creo el searchBar
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>
    
    * Feature/crear pagination component (#7)
    
    * Modifico el layout para ordenar mejor la pagina
    
    * Hago una card
    
    * - instale reactstrap, jquery y popper
    - cree el dropdown
    
    * - creo el searchBar
    
    * - elimino espacios vacios en card y en layout
    
    * - cree el componente pagination
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>
    
    * Feature/crear cards component (#8)
    
    * - instale prop-types
    - cree las cards
    - elimino estilos deprecados
    
    * - hice mejoras a las cards
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>
    
    * - cree el componente boton para filtrar (#9)
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>
    
    * - agregue propTypes al ButtonCuscom
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>
    
    Co-authored-by: Sol0205 <solmargarit3@gmail.com>

--- src/components/ButtonCustom/ButtonCustom.js
+++ src/components/ButtonCustom/ButtonCustom.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
+import React from "react";
+import PropTypes from "prop-types";
 
 function ButtonCustom(props) {
-    return <button className={"btn btn-" + props.type + " p-2 " + (!!props.rounded ? "rounded-circle ": "")  + "btn-" + props.size} >{props.text}</button>;
+  return (
+    <button
+      className={
+        "btn btn-" + props.type +
+        " p-2 " + (!!props.rounded ? "rounded-circle " : "") +
+        "btn-" + props.size
+      }
+    >
+      {props.text}
+    </button>
+  );
 }
 
 ButtonCustom.defaultProps = {
-    type: "info",
-    size: "md",
-    rounded: true
-}
+  type: "info",
+  size: "md",
+  rounded: true,
+};
 
-export default ButtonCustom
+ButtonCustom.propTypes = {
+  type: PropTypes.string,
+  size: PropTypes.string,
+  rounded: PropTypes.bool,
+};
+
+export default ButtonCustom;
