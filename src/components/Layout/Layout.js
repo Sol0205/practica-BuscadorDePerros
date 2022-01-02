7843b50b5785bd22829ec23c3754039436ba2701
Sol0205 <solmargarit3@gmail.com>
2022-01-02 00:40:35 -0300

    - cree el componente boton para filtrar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,6 +4,7 @@ import Cards from '../Cards/Cards.js';
 import DropdownCustom from '../DropdownCustom/DropdownCustom';
 import Pagination from '../Pagination/Pagination';
 import Dogs from '../../utils/constants/MockDogs.json';
+import ButtonCustom from '../ButtonCustom/ButtonCustom';
 
 
 function Layout() {
@@ -18,8 +19,11 @@ function Layout() {
         </div>
       </header>
       <br />
-      <div className="container mt-auto py-3">
-        <DropdownCustom /> 
+      <div className="btn-toolbar" aria-label="Toolbar with button groups">
+        <div className="container mt-auto py-3">
+          <ButtonCustom text="A-Z" type="info" size="lg" rounded={true}/>
+          <DropdownCustom /> 
+        </div>
       </div>
 
       <main className="flex-shrink-0">
